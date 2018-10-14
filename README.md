b23d0208f58aaea6a71bc50f4c421370cbbe7e3f
Toshio Imaoka <pumpkinpie.tea@gmail.com>
2018-10-14 10:13:39 +0900

    change md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # py37
+This is basic files to launch flask 'hello world' 
+on GAE with python 3.7 runtime environment.
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World!!'
 
 
 @app.errorhandler(404)
